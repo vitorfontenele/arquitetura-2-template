71531076a8811bc53aeec09fd5a7441906c25ede
vitor-fontenele <vitor_fontenele@hotmail.com>
2023-02-07 10:36:20 -0300

    Update

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express"
 import { UserBusiness } from "../business/UserBusiness"
+import { BaseError } from "../errors/BaseError"
 
 export class UserController {
     public getUsers = async (req: Request, res: Response) => {
@@ -40,15 +41,11 @@ export class UserController {
             res.status(201).send(output)
         } catch (error) {
             console.log(error)
-    
-            if (req.statusCode === 200) {
-                res.status(500)
-            }
-    
-            if (error instanceof Error) {
-                res.send(error.message)
+
+            if (error instanceof BaseError) {
+                res.status(error.statusCode).send(error.message)
             } else {
-                res.send("Erro inesperado")
+                res.status(500).send("Erro inesperado")
             }
         }
     }
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,17 @@
 import express from 'express'
 import cors from 'cors'
-import { UserController } from './controller/UserController'
-import { AccountController } from './controller/AccountController'
+import { userRouter } from './router/userRouter'
+import { accountRouter } from './router/accountRouter'
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
+app.use("/users", userRouter);
+app.use("/accounts", accountRouter);
+
 app.listen(3003, () => {
     console.log(`Servidor rodando na porta ${3003}`)
 })
 
-const userController = new UserController()
-const accountController = new AccountController()
-
-app.get("/users", userController.getUsers)
-app.post("/users", userController.createUser)
-
-app.get("/accounts", accountController.getAccounts)
-app.get("/accounts/:id/balance", accountController.getAccountBalance)
-app.post("/accounts", accountController.createAccount)
-app.put("/accounts/:id/balance", accountController.editAccountBalance)
